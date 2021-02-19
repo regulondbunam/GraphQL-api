8713c084d132882a62fb16f521cc771c81a654e4
Andrés Gerardo López Almazo <58341665+andresloal@users.noreply.github.com>
2021-02-19 10:35:42 -0600

    minor change on dbConnection and libraries updated

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 //Connection to mongodb with .env credentials on .env file
 const conectarDB = async ()=>{
     try{
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}`,{
+        await mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}`,{
             useUnifiedTopology: true,
             useNewUrlParser: true
         }); 
