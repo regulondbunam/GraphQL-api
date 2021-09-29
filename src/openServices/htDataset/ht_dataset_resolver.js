dad092471fbd534e4d7987c8eec06c01e5e81a60
andresloal <andresg.loal.95@gmail.com>
2021-09-29 17:17:54 -0500

    Feat: Added a first version for HT Dataset services

---
+++ src/openServices/htDataset/ht_dataset_resolver.js
@@ -0,0 +1,49 @@
+/**
+# [HT Dataset Service Resolver]
+	
+## Description
+
+[Resolves the GraphQL Query based on controller's response
+for HT Dataset Service]
+
+## Usage 
+
+```javascript
+import {htDatasetResolvers} from './ht_dataset_resolver'
+```
+
+## Arguments/Parameters
+
+N/A
+
+## Examples
+
+N/A
+
+## Return 
+
+N/A
+
+## Category
+
+"" web service
+
+## License
+
+MIT License
+
+## Author 
+
+RegulonDB Team: Lopez Almazo Andres Gerardo
+**/
+
+/** import the geneController that contains the resolver functions */
+import { htDatasetController } from './ht_dataset_controller';
+import { commonController } from '../common/controller_common_functions';
+
+export const htDatasetResolvers = {
+  Query: {
+    getDatasetsFromSearch: (root, {advSearch}) => htDatasetController.getDatasetsFromSearch(advSearch),
+  },
+};
+ 
