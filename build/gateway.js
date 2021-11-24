8c74e50383cf2152ca6247e450b4e8f3c0f6438b
andresloal <andresg.loal.95@gmail.com>
2021-11-24 16:00:33 -0600

    Fix: Improved test conection to additional services and service now start if they aren't available

--- build/gateway.js
+++ build/gateway.js
@@ -6,14 +6,17 @@ var _express2 = _interopRequireDefault(_express);
 
 var _gatewayPlaygroundOptions = require('./config/gatewayPlaygroundOptions');
 
+var _apolloServerExpress = require('apollo-server-express');
+
+var _gateway = require('@apollo/gateway');
+
+var _apolloFetch = require('apollo-fetch');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const { ApolloServer } = require("apollo-server-express");
-const { ApolloGateway } = require("@apollo/gateway");
-const { createApolloFetch } = require('apollo-fetch');
 require('dotenv').config();
 
-// getting env variables
+// Getting env variables
 const PORT = process.env.GRAPHQL_GATEWAY_PORT || 4001;
 const CLOSED_SERVICES = process.env.GRAPHQL_CLOSED_SERVICES_PORT || 4002;
 const OPEN_SERVICES = process.env.GRAPHQL_OPEN_SERVICES_PORT || 4003;
@@ -22,17 +25,71 @@ const HT_SERVICES_URL = process.env.HT_SERVICES_URL;
 // Setting up the express app
 const app = (0, _express2.default)();
 
+// A first list with local services is defined, additional services will be be added to gateway once they are ready to solve queries
+var gateway;
+var services = [{ name: "openTools", url: `http://localhost:${OPEN_SERVICES}/graphql` }, { name: "closedTools", url: `http://localhost:${CLOSED_SERVICES}/graphql` }];
+
 // app is an instance of EventEmitter, in this case is added a event when emit sends "ready"
 app.on('ready', function () {
   // Stoping interval for trying connect services
   clearInterval(conectionTester);
-  // Services will be be added to gateway once they are ready to solve queries
-  const gateway = new ApolloGateway({
-    serviceList: [{ name: "openTools", url: `http://localhost:${OPEN_SERVICES}/graphql` }, { name: "closedTools", url: `http://localhost:${CLOSED_SERVICES}/graphql` }, { name: "htServices", url: `${HT_SERVICES_URL}` }]
-  });
+  // function to create and start gateway
+  upGraphQLServer(gateway, app);
+});
 
+// Creates an apollo-fetch instance for testing connection to Open and HT Services Server
+let localFetch = (0, _apolloFetch.createApolloFetch)({
+  uri: `http://localhost:${OPEN_SERVICES}/graphql`
+});
+
+let htFetch = (0, _apolloFetch.createApolloFetch)({
+  uri: `${HT_SERVICES_URL}`
+});
+
+// Defining a count
+var count = 0;
+// Adding an interval for looping testing until services are available
+let conectionTester = setInterval(function () {
+  test_services();
+}, 5000);
+
+// function that calls each service to obtain an introspection query response,
+// if an error ocurs for no available connection, a message of wait of retry is showed
+function test_services() {
+  let queryExample = `
+  query {
+    __type(name:"Query"){
+      name
+    }
+  }`;
+  localFetch({
+    query: queryExample
+  }).then(res => {
+    htFetch({
+      query: queryExample
+    }).then(res => {
+      services.push({ name: "htServices", url: `${HT_SERVICES_URL}` });
+      console.log("HT Services are READY");
+      gateway = new _gateway.ApolloGateway({ serviceList: services });
+      app.emit("ready");
+    }).catch(e => {
+      count++;
+      if (count > 5) {
+        gateway = new _gateway.ApolloGateway({ serviceList: services });
+        console.log("Services are starting without HT services");
+        app.emit("ready");
+      } else {
+        console.log(`HT Services aren't ready, trying again in 5 seconds, ${count}/5`);
+      }
+    });
+  }).catch(e => {
+    console.log("Local Services aren't ready, trying again in 5 seconds");
+  });
+}
+
+function upGraphQLServer(gateway, app) {
   //Setting up Apollo Federation server
-  const server = new ApolloServer({
+  const server = new _apolloServerExpress.ApolloServer({
     gateway,
     subscriptions: false,
     playground: _gatewayPlaygroundOptions.playgroundTabs,
@@ -43,6 +100,7 @@ app.on('ready', function () {
     })
   });
 
+  // Applying express app to gateway
   server.applyMiddleware({
     app,
     cors: {
@@ -53,48 +111,4 @@ app.on('ready', function () {
 
   // Starting server on port for listening
   const servExpress = app.listen(PORT, () => console.log(`El servidor esta funcionando en http://localhost:${servExpress.address().port}${server.graphqlPath}`));
-});
-
-// Creates an apollo-fetch instance for testing connection to Open Services Server
-let localFetch = createApolloFetch({
-  uri: `http://localhost:${OPEN_SERVICES}/graphql`
-});
-
-// Creates an apollo-fetch instance for testing connection to Remote HT Services Server
-let htFetch = createApolloFetch({
-  uri: `${HT_SERVICES_URL}`
-});
-
-// Adding an interval for looping testing until services are available
-let conectionTester = setInterval(function () {
-  test_services();
-}, 5000);
-
-// function that calls Open Services API to obtain an introspection query response,
-// if an error ocurs for no available connection, a message of wait of retry is showed
-function test_services() {
-  localFetch({
-    query: `
-    query {
-      __type(name:"Query"){
-        name
-      }
-    }`
-  }).then(res => {
-    htFetch({
-      query: `
-      query {
-        __type(name:"Query"){
-          name
-        }
-      }`
-    }).then(res => {
-      console.log("HT Services READY");
-      app.emit("ready");
-    }).catch(e => {
-      console.log("HT Services aren't ready, trying again in 5 seconds");
-    });
-  }).catch(e => {
-    console.log("Local Services aren't ready, trying again in 5 seconds");
-  });
 }
--- src/config/gatewayPlaygroundOptions.js
+++ src/config/gatewayPlaygroundOptions.js
@@ -3,41 +3,22 @@ export const playgroundTabs = {
       {
         endpoint: 'graphql',
         name: 'updates',
-        query: `# 20/10/2021
-# Added a first version of HT Services
-# This version is for an upcoming release of RegulonDB GraphQL Web Services
-# In case of error or report a bug please contact us
+        query: `# 24/11/2021
+# Added HT Services for dataset, peaks, tfBinding and authorsData
+# In case of error, report a bug or incorrect response please contact us
 
 # Query sample usage
 {
 # Select query, see "docs" tab to get description about 
 # parameters and description
-    getSrnaBy(search:"dicF"){
-    data{
-        _id
-        product{
-        name
-        gene{
-            _id
-            name
-            strand
-        }
-        }
-        regulatoryInteractions{
-        mechanism
-        regulatedEntity{
-            name
-            type
-        }
-        distanceToGene
-        regulatoryBindingSites{
-            leftEndPosition
-            rightEndPosition
-            sequence
-        }
-        }
+getDatasetByID(datasetID: "RHTECOLIBSD00008") {
+    _id
+    objectTested {
+      _id
+      name
+      note
     }
-    }
+  }
 }`,
     },
     ],
